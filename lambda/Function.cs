297b1819f2c6aa64975d5e46e3292aa193d5b3a9
RenatoDidier <renatodidierf@gmail.com>
2026-01-27 21:04:38 -0300

    feat: Colocando a biblioteca faltante no projeto de dotnet do lambda

--- lambda/Function.cs
+++ lambda/Function.cs
@@ -1,4 +1,7 @@
+using System.Text.Json;
+
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
